754c0c40003a2ea2357a9de08f2ecc39df3da081
Sam <samellis1403@gmail.com>
2024-12-08 19:22:31 -0500

    updated

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// template_oyb1xx8
-// service_cuaityq
-// qqO96IaXJEb0zuHc9
-
-
 function contact(event) {
     event.preventDefault();
     emailjs
         .sendForm(
             'service_cuaityq',
-            'template_oyb1xx8',
+            'template_9ysvcaw',
             event.target,
-            'qqO96IaXJEb0zuHc9'
+            'Mmp7Bq8-QgXqI4RfU'
         ).then(() => {
             console.log('this worked')
         })
+        const loading = document.querySelector('.modal_overlay--loading');
+        const success = document.querySelector('.modal_overlay--success');
+        loading.classList += " modal_overlay--visible";
+        setTimeout(() => {
+            console.log('it worked ')
+        }, 500)
 }
