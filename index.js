191a02a3c9b31e23832b94ec8ff14184432d9b45
Sam <samellis1403@gmail.com>
2024-12-10 21:58:44 -0500

    update

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 function contact(event) {
-    event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_cuaityq',
-            'template_9ysvcaw',
-            event.target,
-            'Mmp7Bq8-QgXqI4RfU'
-        ).then(() => {
-            console.log('this worked')
-        })
-        const loading = document.querySelector('.modal_overlay--loading');
-        const success = document.querySelector('.modal_overlay--success');
-        loading.classList += " modal_overlay--visible";
-        setTimeout(() => {
-            console.log('it worked ')
-        }, 500)
+  event.preventDefault();
+  const loading = document.querySelector(".modal_overlay--loading");
+  const success = document.querySelector(".modal_overlay--success");
+  loading.classList += " modal_overlay--visible";
+  emailjs
+    .sendForm(
+      "service_cuaityq",
+      "template_9ysvcaw",
+      event.target,
+      "Mmp7Bq8-QgXqI4RfU"
+    )
+    .then(() => {
+      loading.classList.remove("modal_overlay--visible");
+      success.classList += " modal_overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal_overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on samellis1403@gmail.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    isModalOpen = !isModalOpen;
+    if (isModalOpen) {
+        return document.body.classList.remove("modal--open");
+    }
+    // toggle modal 
+    document.body.classList += " modal--open"
 }
